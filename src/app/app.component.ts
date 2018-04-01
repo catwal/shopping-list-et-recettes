bbb928dc9956c4b6d4218fe47ed58e72c6133c3e
kitekate <cwallez@hotmail.fr>
2018-04-01 16:47:56 +0200

    Rajout de l'API firebase et son min config requis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, ViewChild } from '@angular/core';
 import { Platform, NavController, MenuClose, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+/* import de firebase manuellement */
+import firebase from 'firebase';
 
 import { TabsPage } from '../pages/tabs/tabs';
 import { SigninPage } from '../pages/signin/signin';
@@ -20,6 +22,12 @@ export class MyApp {
   @ViewChild('nav') nav: NavController;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private menuCtrl: MenuController) {
+    /* dans le constructeur on initialise l'application avec firebase et les infos importantes de notre service firebase */
+    firebase.initializeApp({
+      apiKey: "AIzaSyBWYwEooKHBVmkxT1d_gQ-lJe_yQqS8I8E",
+      authDomain: "ionic-http-oauth.firebaseapp.com"
+    })
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -35,8 +43,8 @@ export class MyApp {
     this.nav.setRoot(page);
     this.menuCtrl.close();
   }
-  onLogOut(){
-    
+  onLogOut() {
+
   }
 }
 
