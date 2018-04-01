40b17f6921c7ea670194ea6783ee1a71d49612ac
kitekate <cwallez@hotmail.fr>
2018-04-01 17:19:50 +0200

    config du signin

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 
@@ -11,16 +11,33 @@ import { AuthService } from '../../services/auth.service';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private authService: AuthService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private authService: AuthService, private loadingCtrl: LoadingController, private alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignupPage');
   }
-  onSignup(form: NgForm){
+  onSignup(form: NgForm) {
+    /* présentation du loading - mais faut penser à l'arreter à un moment */
+    const loading = this.loadingCtrl.create({
+      content: 'Enregistrement en cours...'
+    });
+    loading.present();
     this.authService.signup(form.value.email, form.value.password)
-    .then(data => console.log(data))
-    .catch(error => console.log(error));
+      .then(data => {
+        loading.dismiss();
+        console.log(data)
+      })
+      .catch(error => {
+        loading.dismiss();
+        const alert = this.alertCtrl.create({
+          title: 'Erreur d\'enregistrement',
+          message: error.message,
+          buttons: ['Ok']
+        });
+        alert.present();
+        console.log(error)
+      });
     console.log(form.value);
   }
 }
